67ab876454275c58ba20912fd0a5497e6a284726
Danl0gin0ff <140742674+Danl0gin0ff@users.noreply.github.com>
2025-05-25 21:23:15 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+let page = 1;
+const totalPages = 3; // Change this if you have more or fewer pages
+
+function updatePage() {
+  document.getElementById('page').src = `granny_card_pages/page${page}.jpg`;
+}
+
+function nextPage() {
+  if (page < totalPages) {
+    page++;
+    updatePage();
+  }
+}
+
+function prevPage() {
+  if (page > 1) {
+    page--;
+    updatePage();
+  }
+}
